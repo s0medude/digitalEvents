335351a183cad1396b8d289732e38e4426a2d1b2
Aprendiz <Aprendiz@BOGDFPRDC8F418.FormacionSena.RED>
2019-02-08 11:08:09 -0500

    Agregue el controller del grafrico

---
+++ digital_events_repository/src/java/digitalevents/grafrico/controller/GrafricoReservasController.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package digitalevents.grafrico.controller;
+
+import edu.digitalEvents.modelo.dao.IEventoDAO;
+import edu.digitalEvents.modelo.dao.IEventoReservadoViewDAO;
+import edu.digitalEvents.modelo.dao.ISolicitudDAO;
+import edu.digitalEvents.modelo.entities.EventoReservadoView;
+import edu.digitalEvents.modelo.entities.Solicitud;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author Aprendiz
+ */
+@Named(value = "grafricoReservasController")
+@ViewScoped
+public class GrafricoReservasController implements  Serializable{
+
+    /**
+     * Creates a new instance of GrafricoReservasController
+     */
+    @EJB
+    private IEventoReservadoViewDAO IER;
+    @EJB
+    private ISolicitudDAO solicituadDao;
+    private ArrayList<String> fechaReserva = new ArrayList<String>();
+    private ArrayList<Integer> numeroEventos = new ArrayList<Integer>();
+    private ArrayList<EventoReservadoView> eventoreserva;
+
+    public GrafricoReservasController() {
+
+    }
+
+    @PostConstruct
+    public void init() {
+        for (Solicitud e : solicituadDao.findAll()) {
+            //EntityGrafricoZingChar cuen = new EntityGrafricoZingChar(e.getId(), e.getNumeroEvaluacion());
+            SimpleDateFormat nn = new SimpleDateFormat("dd/MM/yyyy");
+            String fecha = nn.format(e.getFechaReserva());
+            fechaReserva.add(fecha);
+            System.out.println(e.getFechaReserva());
+            numeroEventos.add(e.getId());
+        }
+    }
+
+    public String graficoReserva(ArrayList<?> lista, String delimitador) {
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < lista.size(); i++) {
+            if (i > 0) {
+                output.append(delimitador);
+            }
+            // --- Quote strings, only, for JS syntax
+            if (lista.get(i) instanceof String) {
+                output.append("\"");
+            }
+            output.append(lista.get(i));
+            if (lista.get(i) instanceof String) {
+                output.append("\"");
+            }
+        }
+        return output.toString();
+    }
+
+    public ArrayList<String> getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(ArrayList<String> fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    public ArrayList<Integer> getNumeroEventos() {
+        return numeroEventos;
+    }
+
+    public void setNumeroEventos(ArrayList<Integer> numeroEventos) {
+        this.numeroEventos = numeroEventos;
+    }
+
+}

18c9f861866609d42fc2724ec8e4a62caba1f9b0
Usuario <Usuario@10.1.237.150>
2019-03-07 11:02:51 -0500

    Revert "1. Agregue libreria JS"
    This reverts commit 42beb59fc6230a6c9e7df8ee9c796fcbcf9f29e9.

--- digital_events_repository/web/resources/js/main.js
+++ digital_events_repository/web/resources/js/main.js
@@ -15,17 +15,17 @@ $(function(){
             if ( newIndex >= 1 ) {
                 $('.steps ul li:first-child a .step-arrow').remove();
             } else {
-                $('.steps ul li:first-child a').append('<img src="http://localhost:8080/digital_events_repository/resources/img/step-arrow.png" alt="" class="step-arrow"/>');
+                $('.steps ul li:first-child a').append('<img src="resources/img/step-arrow.png" alt="" class="step-arrow"/>');
             }
 
             if ( newIndex === 1 ) {
-                $('.steps ul li:nth-child(2) a').append('<img src="http://localhost:8080/digital_events_repository/resources/img/step-arrow.png" alt="" class="step-arrow"/>');
+                $('.steps ul li:nth-child(2) a').append('<img src="img/step-arrow.png" alt="" class="step-arrow"/>');;
             } else {
                 $('.steps ul li:nth-child(2) a .step-arrow').remove();
             }
 
             if ( newIndex === 2 ) {
-                $('.steps ul li:nth-child(3) a').append('<img src="http://localhost:8080/digital_events_repository/resources/img/step-arrow.png" alt="" class="step-arrow"/>');;
+                $('.steps ul li:nth-child(3) a').append('<img src="img/step-arrow.png" alt="" class="step-arrow"/>');;
             } else {
                 $('.steps ul li:nth-child(3) a .step-arrow').remove();
             }
@@ -33,8 +33,8 @@ $(function(){
         }
     });
     // Create Steps Image
-    $('.steps ul li:first-child a').append('<img src="http://localhost:8080/digital_events_repository/resources/img/step-1.png" alt=""/>').append('<img src="http://localhost:8080/digital_events_repository/resources/img/step-arrow.png" alt="" class="step-arrow"/>');
-    $('.steps ul li:nth-child(2) a').append('<img src="http://localhost:8080/digital_events_repository/resources/img/step-2.png" alt=""/>');
-    $('.steps ul li:last-child a').append('<img src="http://localhost:8080/digital_events_repository/resources/img/step-3.png" alt=""/>');
+    $('.steps ul li:first-child a').append('<img src="img/step-1.png" alt=""/>').append('<img src="img/step-arrow.png" alt="" class="step-arrow"/>');
+    $('.steps ul li:nth-child(2) a').append('<img src="img/step-2.png" alt=""/>');
+    $('.steps ul li:last-child a').append('<img src="img/step-3.png" alt=""/>');
 });
 
